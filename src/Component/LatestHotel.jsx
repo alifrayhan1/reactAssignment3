1945d96a395335944bf21707a1a173cc701d7639
alifrayhan <alifrayhan92@gmail.com>
2025-02-14 23:38:28 +0600

    (fix) LatestHotel.jsx Card.jsx import problem

--- src/Component/LatestHotel.jsx
+++ src/Component/LatestHotel.jsx
@@ -1,4 +1,4 @@
-import Card from "../Reuse/card";
+import Card from "./C:/webJourney/reactAsignment3/src/Reuse/Card.jsx";
 import LatestHotelInfo from "../Source/LatestHotelInfo";
 
 function shuffleArray(array) {
